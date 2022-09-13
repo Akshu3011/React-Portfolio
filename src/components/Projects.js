8f97ce5bced85817cd775cdcb28f08deb7deeab1
Akshata Kumbhar <105023659+Akshu3011@users.noreply.github.com>
2022-09-13 00:10:52 -0700

    updated the portfolio with page components and Navbar

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -10,21 +10,13 @@ const Projects = () => {
                 <span className='line'></span>
                 <div className='content'>
                     <div className='card'>
-                        <p>It is not every day that you come across a passionate and trustworthy financial advisor. John Doe is true professional who does his work really well. Thanks John!</p>
-                        <p><span>Johnson.M.J.</span></p>
-                        <p>Director of "Financial Times"</p>
+                      
                     </div>
                     <div className='card'>
                         
-                        <p>In just 2 very short meetings with John he managed to find the solution personally catered to my situation and expectations. Punctual, well informed and very reliable.</p>
-                        <p><span>Carol Harper</span></p>
-                        <p>Director at Risktec Solutions Ltd</p>
-                    </div>
+                      </div>
                     <div className='card'>
-                        <p>A very professional financial advisor, who is true to his word. John has demonstrated a high amount of integrity in the time I have known him, and he is fast to respond to my concerns.</p>
-                        <p><span>Snow.J.R.</span></p>
-                        <p>Managing Director of BPW Global</p>
-                    </div>
+                      </div>
                 </div>
             </div>
         </div>
